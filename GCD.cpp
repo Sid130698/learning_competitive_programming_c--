502cd4d63fc1e7885dc7f01583dca65bbcabdf81
Sid130698 <sidsingh130698@gmail.com>
2021-04-04 04:36:54 +0530

    GCD ADDED

---
+++ GCD.cpp
@@ -0,0 +1,43 @@
+/*we are going to use EUCLIDS Algorithm,which is
+GCD(a,b){a>b}=> GCD(b,a%b)
+ex: GCD(16,10)=>GCD(10,6)=>GCD(6,4)=>GCD(4,2)=>GCD(2,0)=> 2 Ans.
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+// int gcd(int a,int b){
+//     int high,low;
+//     if(a>b){
+//         high=a;
+//         low=b;
+//     }
+//     else{
+//         low=a;
+//         high=b;
+//     }
+//     if(high%low==0){
+//         return low;
+//     }
+//     else
+//     gcd(low,high%low);
+// }
+ 
+ int gcd(int a,int b){
+     if(a<b)
+     return gcd(b,a);
+     if(b==0)
+     return a;
+     return gcd(b,a%b);
+ }
+ int main(){
+
+    int a,b;
+    cin>>a>>b;
+    int Gcd=gcd(a,b);
+    cout<<Gcd;
+
+
+    return 0;
+ }
